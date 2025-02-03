43c3411ed2c54db3fd495d692e51acdceab77e5b
Eli Heuer <elih@protonmail.com>
2025-02-02 17:01:50 -0800

    Break main application up into 3 smaller parts

---
+++ src/theme.rs
@@ -0,0 +1,11 @@
+use bevy::prelude::Color;
+
+// UI Colors
+pub const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+pub const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+pub const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+// Path Drawing
+pub const NUM_POINTS: usize = 32;
+pub const POINT_RADIUS: f32 = 5.0;
+pub const PATH_COLOR: Color = Color::srgb(0.8, 0.0, 0.0); 
