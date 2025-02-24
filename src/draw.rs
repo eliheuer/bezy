c04e13e1d64d8c870fbfee560cc5fae580df5f5d
Eli Heuer <elih@protonmail.com>
2025-02-24 13:53:00 -0800

    Adding point rendering

--- src/draw.rs
+++ src/draw.rs
@@ -115,36 +115,74 @@ pub fn draw_glyph_points_system(
     app_state: Res<AppState>,
     viewports: Query<&ViewPort>,
 ) {
-    // Get the primary viewport
-    if let Ok(viewport) = viewports.get_single() {
-        // Only proceed if we have a font loaded
-        if app_state.workspace.font.ufo.font_info.is_some() {
-            // Get the 'H' glyph (Unicode codepoint U+0048)
-            let codepoint = 0x0048u32; // 'H' character
+    // Add debug message at the start so we know this system is being called
+    info!("DRAW SYSTEM: draw_glyph_points_system is running...");
+    
+    // Get the primary viewport or create a default one if none exists
+    let viewport = match viewports.get_single() {
+        Ok(viewport) => *viewport,
+        Err(_) => ViewPort::default(),
+    };
+    
+    // Print font info for debugging
+    println!("Font family: {}", app_state.workspace.info.family_name);
+    println!("Font style: {}", app_state.workspace.info.style_name);
+    
+    // Check if we can get the default layer
+    println!("Checking for default layer in the font...");
+    match app_state.workspace.font.ufo.get_default_layer() {
+        Some(default_layer) => {
+            println!("Found default layer, trying to access glyph 'H'");
             
-            // Try to find the glyph with this codepoint
-            if let Some(glyph) = find_glyph_by_codepoint(&app_state.workspace.font.ufo, codepoint) {
-                // Draw the points for this glyph
-                draw_glyph_points(&mut gizmos, *viewport, &glyph);
-            } else {
-                info!("Glyph for codepoint U+{:04X} not found", codepoint);
+            // Try to get the 'H' glyph directly by name
+            let glyph_name = norad::GlyphName::from("H");
+            
+            match default_layer.get_glyph(&glyph_name) {
+                Some(glyph) => {
+                    println!("SUCCESS: Found glyph 'H', drawing points...");
+                    
+                    // Draw the points
+                    draw_glyph_points(&mut gizmos, viewport, glyph);
+                }
+                None => {
+                    println!("Glyph 'H' not found by name, trying 'h'");
+                    
+                    // Try with lowercase 'h'
+                    let lowercase_name = norad::GlyphName::from("h");
+                    match default_layer.get_glyph(&lowercase_name) {
+                        Some(glyph) => {
+                            println!("Found glyph 'h' instead, drawing points...");
+                            draw_glyph_points(&mut gizmos, viewport, glyph);
+                        }
+                        None => {
+                            println!("Couldn't find 'h' either, trying common glyph names...");
+                            
+                            // Try with some other common glyphs
+                            let common_glyphs = ["A", "a", "O", "o", "space", ".notdef"];
+                            let mut found = false;
+                            
+                            for glyph_name_str in common_glyphs.iter() {
+                                let name = norad::GlyphName::from(*glyph_name_str);
+                                if let Some(glyph) = default_layer.get_glyph(&name) {
+                                    println!("Found glyph '{}', drawing points...", glyph_name_str);
+                                    draw_glyph_points(&mut gizmos, viewport, glyph);
+                                    found = true;
+                                    break;
+                                }
+                            }
+                            
+                            if !found {
+                                println!("WARNING: Could not find any common test glyphs");
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
-}
-
-/// Find a glyph by Unicode codepoint
-fn find_glyph_by_codepoint(ufo: &norad::Ufo, codepoint: u32) -> Option<norad::Glyph> {
-    // Iterate through all glyphs and find one with the matching codepoint
-    for (glyph_name, glyph) in ufo.iter_glyphs() {
-        if let Some(codepoints) = &glyph.codepoints {
-            if codepoints.contains(&codepoint) {
-                info!("Found glyph '{}' for codepoint U+{:04X}", glyph_name, codepoint);
-                return Some(glyph.clone());
-            }
+        None => {
+            println!("WARNING: No default layer found in the font");
         }
     }
-    None
 }
 
 /// Draw points from a glyph
@@ -153,33 +191,71 @@ fn draw_glyph_points(
     viewport: ViewPort,
     glyph: &norad::Glyph,
 ) {
+    println!("Drawing glyph '{}' at scale {:.2}", glyph.name, viewport.zoom);
+    
     // Only proceed if the glyph has an outline
     if let Some(outline) = &glyph.outline {
         // Log information about the glyph for debugging
-        info!("Drawing points for glyph '{}' with {} contours", 
-              glyph.name, outline.contours.len());
+        println!("Glyph '{}' has {} contours", glyph.name, outline.contours.len());
         
         // Color for drawing points
         let point_color = Color::srgba(0.0, 0.5, 1.0, 1.0);
+        let control_point_color = Color::srgba(1.0, 0.3, 0.3, 0.8); // Red for control points
         
         // Iterate through all contours
         for (contour_idx, contour) in outline.contours.iter().enumerate() {
-            info!("Contour {} has {} points", contour_idx, contour.points.len());
+            println!("Contour {} has {} points", contour_idx, contour.points.len());
             
             // Draw each point in the contour
             for (point_idx, point) in contour.points.iter().enumerate() {
                 let point_pos = (point.x as f32, point.y as f32);
                 let screen_pos = viewport.to_screen(DPoint::from(point_pos));
                 
+                // Use different sizes and colors based on point type
+                let (size, color) = match point.typ {
+                    norad::PointType::Move | norad::PointType::Line | norad::PointType::Curve => 
+                        (6.0, point_color),
+                    norad::PointType::OffCurve => 
+                        (4.0, control_point_color),
+                    _ => (4.0, point_color)
+                };
+                
                 // Draw a circle for each point
-                gizmos.circle_2d(screen_pos, 4.0, point_color);
+                gizmos.circle_2d(screen_pos, size, color);
+                
+                // Draw a line connecting on-curve and off-curve points
+                if point_idx > 0 {
+                    let prev_point = &contour.points[point_idx - 1];
+                    let prev_pos = (prev_point.x as f32, prev_point.y as f32);
+                    let prev_screen_pos = viewport.to_screen(DPoint::from(prev_pos));
+                    
+                    // Draw line between points
+                    let line_color = Color::srgba(0.5, 0.5, 0.5, 0.7);
+                    gizmos.line_2d(prev_screen_pos, screen_pos, line_color);
+                }
                 
                 // Useful debug info
-                info!("Point {}: ({}, {}), type: {:?}", 
-                      point_idx, point.x, point.y, point.typ);
+                println!("Point {}: ({}, {}), type: {:?}", 
+                       point_idx, point.x, point.y, point.typ);
+            }
+            
+            // Connect the last point to the first to close the contour
+            if !contour.points.is_empty() {
+                let first_point = &contour.points[0];
+                let last_point = &contour.points[contour.points.len() - 1];
+                
+                let first_pos = (first_point.x as f32, first_point.y as f32);
+                let last_pos = (last_point.x as f32, last_point.y as f32);
+                
+                let first_screen_pos = viewport.to_screen(DPoint::from(first_pos));
+                let last_screen_pos = viewport.to_screen(DPoint::from(last_pos));
+                
+                // Draw closing line
+                let line_color = Color::srgba(0.5, 0.5, 0.5, 0.7);
+                gizmos.line_2d(last_screen_pos, first_screen_pos, line_color);
             }
         }
     } else {
-        info!("Glyph '{}' has no outline", glyph.name);
+        println!("Glyph '{}' has no outline", glyph.name);
     }
 }
