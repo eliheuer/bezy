ac91b91b33325ee140ab1f54b680bebebca23ffb
Eli Heuer <elih@protonmail.com>
2025-03-18 20:52:30 -0700

    Fix knife tool bugs

--- src/ui_interaction.rs
+++ src/ui_interaction.rs
@@ -19,15 +19,16 @@ impl Plugin for UiInteractionPlugin {
 /// System to detect when the mouse is hovering over any UI element
 /// This will set the UiHoverState.is_hovering_ui to true when hovering UI
 pub fn detect_ui_hover(
-    interaction_query: Query<&Interaction>,
+    interaction_query: Query<(&Interaction, Option<&Node>)>,
     mut ui_hover_state: ResMut<UiHoverState>,
 ) {
     // Reset hover state at the beginning of each frame
     ui_hover_state.is_hovering_ui = false;
     
     // Check if any UI element is being hovered or interacted with
-    for interaction in interaction_query.iter() {
-        if matches!(interaction, Interaction::Hovered | Interaction::Pressed) {
+    for (interaction, node) in interaction_query.iter() {
+        // Make sure we're only considering actual UI elements (with Node component)
+        if node.is_some() && matches!(interaction, Interaction::Hovered | Interaction::Pressed) {
             ui_hover_state.is_hovering_ui = true;
             return;
         }
